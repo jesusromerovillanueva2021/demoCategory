d83371488fd2f5cacbbe31362cbb05623a52761d
jesus <jesusromerovillanueva@gmail.com>
2022-01-06 17:04:40 -0500

    first commit

---
+++ src/main/java/jrv/nttdata/demoCategory/DemoCategoryApplication.java
@@ -0,0 +1,13 @@
+package jrv.nttdata.demoCategory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCategoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCategoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jrv/nttdata/demoCategory/DemoCategoryApplicationTests.java
@@ -0,0 +1,13 @@
+package jrv.nttdata.demoCategory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCategoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
